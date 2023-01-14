ec4c53dddb0aac64d3700a5b4d2fda801a7e7f69
root-kawaii <teo.regge.99@gmail.com>
2023-01-14 17:10:41 +0100

    init eheh get it?

--- init.sh
+++ init.sh
@@ -8,7 +8,6 @@ sudo pacman -Syu gdm
 
 sudo pacman -Syu visual-studio
 
-'''
 sudo pacman -S --needed base-devel git
 
 sudo git clone https://aur.archlinux.org/yay.git
@@ -18,7 +17,6 @@ cd yay
 makepkg -si
 
 cd ..
-'''
 
 yay -S visual-studio-code-bin
 
@@ -30,4 +28,13 @@ sudo chsh -s $(which zsh)
 
 sudo pacman -Syu curl   
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+sudo pacman -S python-pip
+
+sudo pip3 install pywal
+
+# Define the wallpaper path
+wallpaper="/home/root-kawaii/Pictures/14.png"
+
+wal -i $wallpaper
